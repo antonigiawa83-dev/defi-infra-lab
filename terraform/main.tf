e4db6a78a2dcc3c9e38dae2a1546682588b31bd6
antonigiawa83-dev <antonigiawa83@gmail.com>
2026-02-02 16:13:22 +0700

    Add AWS EC2 instance resource for app server
    Traffic meningkat 10x. Upgrading instance type untuk handle load sementara.

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,13 @@ terraform {
 provider "aws" {
   region  = "ap-southeast-1"
 }
+
+# --- INI KODE BARU YANG DITAMBAHKAN ---
+resource "aws_instance" "app_server" {
+  ami           = "ami-0b93ce03dcbcb99ea" # ID mesin Ubuntu
+  instance_type = "t2.micro"              # Ukuran mesin (Kecil)
+
+  tags = {
+    Name = "Paradex-Node"
+  }
+}
